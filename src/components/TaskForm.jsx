d4085463835eb0c71f3144e0e643606447bb26ae
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2024-01-04 17:31:18 -0600

    terminado

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -22,8 +22,11 @@ function TaskForm() {
       
 
     return (
-      <form onSubmit={handleSubmit}>
+      <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
         <input
+          className="bg-slate-300 p-3 w-full mb-2"
           placeholder="Escribe tu tarea"
           onChange={(e) => {
             setTitle(e.target.value);
@@ -32,14 +35,18 @@ function TaskForm() {
           autoFocus
         />
         <textarea
+          className="bg-slate-300 p-3 w-full mb-2"
           placeholder="Escribe la descripcion de la tarea"
           value={description}
           onChange={(e) => {
             setDescription(e.target.value);
           }}
         ></textarea>
-        <button>Guardar</button>
+        <button
+        className="bg-indigo-500 px-3 py-1 text-white"
+        >Guardar</button>
       </form>
+      </div>
     );
 }
 
